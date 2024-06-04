11bf8be59dc35717736f73fe4d575ce264eb92cb
FabianCuntz <39773720+KastenBrot@users.noreply.github.com>
2024-06-04 19:32:11 +0000

    Remove node_modules and added images

---
+++ public/index.ts
@@ -0,0 +1,63 @@
+/* Hero Images*/
+import Logo from "@/public/logo.svg";
+import LogoWithText from "@/public/logo_with_text.svg";
+import Hero from "@/public/Hero.png";
+
+/* Social Media Icons */
+import Twitter from "@/public/twitter.svg";
+import Instagram from "@/public/instagram.svg";
+import Facebook from "@/public/facebook.svg";
+import Discord from "@/public/discord.svg";
+import Whatsapp from "@/public/whatsapp.svg";
+import Twitch from "@/public/twitch.svg";
+import Youtube from "@/public/youtube.svg";
+
+/* Parner Logos */
+import BARMER from "@/public/LOGO_BARMER.svg";
+import CSStuttgart from "@/public/LOGO_CSS.svg";
+import HAM from "@/public/LOGO_HAM.svg";
+import INNOTEC from "@/public/LOGO_INNOTEC.svg";
+import SMZ from "@/public/LOGO_SMZ.svg";
+
+import Overwatch from "@/public/overwatch.jpg";
+import Valorant from "@/public/valorant.jpg";
+import CSGO from "@/public/csgo.jpg";
+import Apex from "@/public/apex.jpg";
+import Dota2 from "@/public/dota2.jpg";
+import RocketLeague from "@/public/rocketlegue.jpg";
+import LeagueofLegends from "@/public/leagueoflegends.jpg";
+import F1 from "@/public/f1.jpg";
+
+import ValorantLogo from "@/public/valologo.png";
+
+export {
+  /* Hero Images */
+  Logo,
+  LogoWithText,
+  Hero,
+
+  /* Social Media Icons */
+  Instagram,
+  Facebook,
+  Twitter,
+  Discord,
+  Whatsapp,
+  Twitch,
+  Youtube,
+
+  /* Parner Logos */
+  BARMER,
+  CSStuttgart,
+  HAM,
+  INNOTEC,
+  SMZ,
+  Overwatch,
+  Valorant,
+  CSGO,
+  Apex,
+  Dota2,
+  RocketLeague,
+  LeagueofLegends,
+  F1,
+  ValorantLogo,
+};
