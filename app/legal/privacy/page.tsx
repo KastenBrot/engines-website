00a412613ffdf836bed85e05655c779e16e0007d
FabianCuntz <39773720+KastenBrot@users.noreply.github.com>
2024-06-10 21:15:20 +0000

    feat: Add (hardcoded) statutes, pricay policy and imprint

---
+++ app/legal/privacy/page.tsx
@@ -0,0 +1,85 @@
+export default async function Legal({ params }: { params: { page: string } }) {
+    return (
+        <section className="flex flex-col">
+            <div className="flex justify-center h-1/3">
+                <h1 className="absolute text-6xl mt-32 text-transparent bg-clip-text bg-gradient-to-r from-engines">Datenschutzerklärung</h1>
+            </div>
+            <div className="flex flex-col gap-4 p-8 mt-44 relative lg:mx-96 md:mx-32 mx-12">
+                <h1 className="text-2xl">Datenschutzhinweis</h1>
+
+                <h2 className="mt-5 mb-0 text-lg">Allgemeines</h2>
+                <p>
+                    Der Schutz personenbezogener Daten ist uns ein wichtiges Anliegen. Wir erheben und verarbeiten personenbezogene Daten von Besucherinnen und Besuchern unserer Internetseiten unter Beachtung der geltenden datenschutzrechtlichen Bestimmungen der EU-Datenschutzgrundverordnung (EU-DSGVO) sowie des Landesdatenschutzgesetzes (LDSG). Die genannten Vorschriften betreffen personenbezogene Daten. Personenbezogene Daten sind Einzelangaben über persönliche und sachliche Verhältnisse einer bestimmten oder bestimmbaren natürlichen Person. Dies können bei einer Kontaktaufnahme per Post, E-Mail, oder Web-Formular beispielsweise der Name, die Anschrift oder die E-Mail-Adresse sein.
+                    Hiermit unterrichten wir Sie über Art, Umfang und Zwecke der Erhebung und Verwendung derjenigen Daten, die während Ihres Besuchs auf unserer Homepage erfasst und wie sie genutzt werden. Sie können diese Unterrichtung jederzeit auf unserer Webseite abrufen.
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Datenerhebung und -verarbeitung bei Zugriffen aus dem Internet</h2>
+
+                <p>
+                    Während des Aufenthalts auf unseren Internetseiten werden nach einem Einloggen dem System personenbezogene Daten bereitgestellt. Jeder Zugriff auf unsere Homepage und jeder Abruf einer auf der Homepage hinterlegten Datei wird protokolliert. Diese Daten werden nicht öffentlich auf unseren Internetseiten dargestellt, sondern können in verschiedenen Erweiterungen verarbeitet werden. Die Speicherung dient internen systembezogenen und statistischen Zwecken. Protokolliert werden: Name der abgerufenen Datei, Datum und Uhrzeit des Abrufs, übertragene Datenmenge, Meldung über erfolgreichen Abruf, Browser Version des zugreifenden Hostsystems; Betriebssystem Version des zugreifenden Hostsystems. Die Verarbeitung dieser Daten erfolgt zu dem Zweck, die Nutzung unserer Internetseiten zu ermöglichen (Verbindungsaufbau) sowie zu Zwecken der Systemsicherheit, der technischen Administration der Netzinfrastruktur und zur Optimierung des Internetangebots. Zusätzlich werden die IP Adressen der anfragenden Rechner protokolliert. <br />
+                    Weitergehende personenbezogene Daten werden nur erfasst, wenn Sie diese Angaben freiwillig, etwa im Rahmen einer Anfrage über das Webformular, machen. Das Vorhalten der Daten erlischt mit dem Abmelden oder dem Schließen des Webbrowsers. Über die vorstehend genannten Fälle hinaus werden personenbezogene Daten nicht verarbeitet, es sei denn, Sie willigen ausdrücklich in eine weitergehende Verarbeitung ein. Sie haben jederzeit die Möglichkeit, eine erteilte Einwilligung für die Zukunft zu widerrufen.
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Kontaktaufnahme per Web-Formular:</h2>
+
+                <p>
+                    Bei der Kontaktaufnahme per E-Mail oder Web-Formular gilt: diese Daten werden von uns unter Beachtung der Vorschriften der EU-DSGVO durch technische und organisatorische Maßnahmen geschützt. Mit der Übermittlung stimmen Sie der zweckgebundenen Speicherung, Verarbeitung und Benutzung Ihrer personenbezogenen Daten zu. Wir verwenden die Daten nur zur Beantwortung Ihrer Anfragen und für die technische Administration. Gemäß Artikel 15 EU-DSGVO haben Sie das Recht auf Auskunft hinsichtlich Ihrer bei uns gespeicherten Daten sowie gemäß Artikel 16, 17 und 18 EU-DSGVO haben Sie jederzeit das Recht auf Berichtigung, Löschung und Sperrung dieser personenbezogenen Daten. Im Rahmen des gesetzlichen Auskunftsanspruchs erhalten Sie daher jederzeit ohne Angaben von Gründen kostenfrei Auskunft über Ihre bei uns gespeicherten personenbezogenen Daten. Sie können jederzeit die uns erteilten Einwilligung zur Datenerhebung und Verwendung ohne Angaben von Gründen widerrufen.
+                    <br />
+                    In diesem Fall wenden Sie sich bitte an:
+                    <br />
+                    <b>Fabian Cuntz</b>
+                    <br />
+                    f.cuntz@engines-stuttgart.de
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Nutzung und Weitergabe personenbezogener Daten</h2>
+
+                <p>
+                In bestimmten Fällen ist für die Nutzung unseres Internetangebots die Angabe personenbezogener Daten wie Name, Anschrift oder E-Mail-Adresse erforderlich. Falls dies der Fall ist, werden jeweils direkt an der gleichen Stelle Art, Umfang und Zweck der Datenerhebung und -verarbeitung genannt. <br />
+                Übermittlungen personenbezogener Daten an staatliche Einrichtungen und Behörden erfolgen nur im Rahmen zwingender nationaler Rechtsvorschriften oder wenn die Weitergabe im Fall von Angriffen auf unsere Netzinfrastruktur zur Rechts- oder Strafverfolgung erforderlich ist. Eine Weitergabe zu anderen Zwecken an Dritte findet nicht statt.
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Einwilligung in weitergehende Datennutzung</h2>
+
+                <p>
+                    Die Nutzung bestimmter Angebote auf unseren Webseiten wie etwa von Newsletter-Abonnements erfordert die Angabe und weitergehende Verarbeitung personenbezogener Daten, beispielsweise eine längerfristige Speicherung von E-Mail-Adressen oder postalischen Anschriften. Mit Angabe der erfragten Daten und Übersendung willigen Sie in deren
+                    Verarbeitung ein. Sie können diese Einwilligung gegenüber dem im Impressum genannten Betreiber der Webseite mit Wirkung für die Zukunft widerrufen.
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Einwilligung in weitergehende Datennutzung</h2>
+
+                <p>
+                    Wir setzen technische und organisatorische Sicherheitsmaßnahmen ein, um Ihre durch uns verwalteten personenbezogenen Daten gegen zufällige oder vorsätzliche Manipulationen, Verlust, Zerstörung oder gegen den Zugriff unberechtigter Personen zu schützen. Unsere Sicherheitsmaßnahmen werden entsprechend der technologischen Entwicklung fortlaufend verbessert.
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Einsatz von Cookies</h2>
+
+                <p>
+                Beim Aufruf einzelner Internetseiten werden sogenannte temporäre Cookies verwendet. Diese Session-Cookies beinhalten keine personenbezogenen Daten und werden nach Ablauf der Sitzung mit dem Schließen Ihres Browsers automatisch gelöscht. <br />
+                Natürlich können Sie unsere Internetseiten auch ohne Cookies betrachten. Die meisten Browser akzeptieren Cookies jedoch automatisch. Sie können das Speichern von Cookies verhindern, indem Sie dies in Ihren Browser-Einstellungen festlegen. Wenn Sie keine Cookies akzeptieren, kann dies zu Funktionseinschränkungen unserer Angebote führen.
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Links zu Webseiten anderer Anbieter</h2>
+
+                <p>
+                    Die vorliegende Datenschutzerklärung gilt für die Internetseiten der Domäne www.engines-stuttgart.de. Soweit über Querverweise (Links) auf Inhalte anderer Anbieter verwiesen wird, kann deren Datenerhebung und Datennutzung nach anderen als den oben dargestellten Grundsätzen erfolgen. <br />
+                    Hinweise zur Verantwortung für die bereitgestellten Inhalte einer Internetseite finden Sie im jeweiligen Impressum.
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Auskunftsrecht und Kontaktdaten</h2>
+
+                <p>
+                Ihnen steht ein Auskunftsrecht bezüglich der über Sie gespeicherten personenbezogenen Daten und ferner ein Recht auf Berichtigung unrichtiger Daten, Sperrung und Löschung zu. <br />
+                Sollten Sie eine Korrektur oder Löschung von Daten wünschen oder weitergehende Fragen zur Verwendung der Daten haben, kontaktieren Sie bitte die im Impressum der genutzten Internetpräsenz genannten Betreiber (Redaktion).
+                </p>
+
+                <h2 className="mt-5 mb-0 text-lg">Aktualität und Gültigkeit der Datenschutzerklärung</h2>
+
+                <p>
+                    Mit der Nutzung unserer Internetpräsenz willigen Sie in die vorab beschriebene Datenverwendung ein. Die vorliegende Datenschutzerklärung ist aktuell gültig und datiert vom 06.10.2024. <br />
+                    Durch die Weiterentwicklung unserer Internetseiten oder die Implementierung neuer Technologien kann es notwendig werden, diese Datenschutzerklärung zu ändern.Wir behalten uns vor, die Datenschutzerklärung jederzeit mit Wirkung für die Zukunft zu ändern.
+                </p>
+            </div>
+        </section>
+    );
+}
