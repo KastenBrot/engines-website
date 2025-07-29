37b874e70e5c6b2264efabb4990f244001dc33fb
KastenBrot <39773720+KastenBrot@users.noreply.github.com>
2025-07-29 17:04:36 +0000

    feat: fix some spacings, add a burger and fix the hero iamge

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -16,10 +16,10 @@ const HeroSection = (props: Props) => {
   return (
     <section
       id="#home"
-      className="flex flex-col h-[50vh] sm:h-screen lg:h-[80vh] lg:flex-row customGradient"
+      className="relative flex flex-col h-[50vh] sm:h-screen lg:h-[80vh] lg:flex-row customGradient"
     >
       {/* IMAGE CONTAINER */}
-      <div className="md:h-1/2 lg:h-full lg:w-[60%] lg:order-2 heroGradient lg:mr-[-5%] relative">
+      <div className="absolute inset-0 heroGradient">
         <Image
           src={HeroOriginal}
           alt="Hero Section Image"
@@ -30,11 +30,11 @@ const HeroSection = (props: Props) => {
                50vw"
           placeholder="blur"
           priority
-          className="object- md:object-cover"
+          className="object-cover w-full h-full filter blur-sm"
         />
       </div>
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-[40%] flex flex-col gap-4 2xl:gap-8 items-start justify-center lg:order-1 lg:pl-20 2xl:pl-40 absolute md:relative m-4 md:m-0">
+      <div className="relative z-10 h-1/2 lg:h-full lg:w-[40%] flex flex-col gap-4 2xl:gap-8 items-start justify-center lg:order-1 lg:pl-20 2xl:pl-40 m-4 md:m-4 sm-m-2">
         <span className="text-base 2xl:text-3xl text-primaryGreen font-inter font-semibold">
           ENGINES STUTTGART e.V.
         </span>
