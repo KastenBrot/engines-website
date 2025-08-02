36ff65c923cf784b3e60f92a0fa6e99052938eac
KastenBrot <39773720+KastenBrot@users.noreply.github.com>
2025-08-02 10:58:20 +0000

    feat: adjust eSport spelling to E-Sport

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ const About = (props: Props) => {
     <section id="about" className="p-0 md:p-8 lg:p-8 bg-secondary ">
         <div className="text-center p-4 md:p-8 lg:p-8 mx-auto w-[95vw] md:w-5/6 lg:w-5/6 bg-white rounded text-primary">
             <h1 className="text-xl md:text-2xl">
-                <span className="text-6xl text-primaryGreen font-serif">"</span> Wir machen es uns zur Aufgabe, den eSport in der Metropolregion Stuttgart und darüber hinaus zu fördern und interessierten Spieler*innen ein professionelles Umfeld für ihre Entwicklung zu bieten.
+                <span className="text-6xl text-primaryGreen font-serif">"</span> Wir machen es uns zur Aufgabe, den E-Sport in der Metropolregion Stuttgart und darüber hinaus zu fördern und interessierten Spieler*innen ein professionelles Umfeld für ihre Entwicklung zu bieten.
             </h1>
             <div className="flex justify-end w-full">
               <Button
