7ad821c43d57f20382a066ef3e948bc68e743fd2
KastenBrot <39773720+KastenBrot@users.noreply.github.com>
2025-12-25 21:19:33 +0000

    feat: add link to members area

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Games from "@/components/Games";
 import HeroSection from "@/components/HeroSection";
 import Partners from "@/components/Partners";
 import About from "@/components/About";
+import MembersSection from "@/components/MembersSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Partners />
       <About />
       <Games />
+      <MembersSection />
       <Partners />
     </>
   );
